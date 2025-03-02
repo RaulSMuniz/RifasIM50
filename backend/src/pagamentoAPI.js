1a9de68cd26af2ab12dbf46e9ff10e75a89f5c28
RaulSMuniz <raulanthonymuniz@gmail.com>
2025-03-02 17:11:12 -0300

    Adicionado método post para geração da cobrança utilizando o token de forma inicial.

--- backend/src/pagamentoAPI.js
+++ backend/src/pagamentoAPI.js
@@ -3,10 +3,8 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const axios = require('axios');
-const fetch = require("node-fetch");
-
 const certPath = fs.readFileSync(
-    path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
+    path.resolve(__dirname, `../../certs/${process.env.GN_CERT}`)
 );
 const https = require("https");
 
@@ -30,4 +28,29 @@ axios({
         grant_type: "client_credentials",
 
     }
-}).then(console.log)
+}).then((response) => {
+    const accessToken = response.data?.access_token;
+
+    const endpoint = `${process.env.GN_ENDPOINT}/v2/cob`
+
+    const dataCob = {
+        calendario: {
+            expiracao: 3600
+        },
+        valor: {
+            original: '10.50'
+        },
+        chave: '126bec4a-2eb6-4b79-a045-78db68412899',
+        solicitacaoPagador: 'Cobrança dos serviços prestados.'
+    };
+
+    const config = {
+        httpsAgent: agent,
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    axios.post(endpoint, dataCob, config).then(response => console.log(response.data));
+})
